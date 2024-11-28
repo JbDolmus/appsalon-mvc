7173741ae42c8d1d6990a60935f27f8ef2ba04e5
JBDolmus <jbautista.dormo.corea@gmail.com>
2024-11-27 22:26:02 -0600

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -19,15 +19,15 @@ if (count($citas) === 0) {
 ?>
 
 <div id="citas-admin">
-    <ul class="citas">
-        <?php
-        $idCita = 0;
-        foreach ($citas as $key => $cita) {
-            if ($idCita !== $cita->id) {
-                $total = 0;
-
-        ?>
-                <li>
+    <ul class="citas">   
+            <?php 
+                $idCita = 0;
+                foreach( $citas as $key => $cita ) {
+   
+                    if($idCita !== $cita->id) {
+                        $total = 0;
+            ?>
+            <li>
                     <p>ID: <span><?php echo $cita->id; ?></span></p>
                     <p>Hora: <span><?php echo $cita->hora; ?></span></p>
                     <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
@@ -35,31 +35,28 @@ if (count($citas) === 0) {
                     <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
 
                     <h3>Servicios</h3>
-                <?php
+            <?php 
                 $idCita = $cita->id;
-            } //fin de if 
-            $total += $cita->precio;
-                ?>
-                <p class="servicio"><?php echo $cita->servicio . ": $" . $cita->precio; ?></p>
-
-
-                <?php
+            } // Fin de IF 
+                $total += $cita->precio;
+            ?>
+                    <p class="servicio"><?php echo $cita->servicio . ": $" . $cita->precio; ?></p>
+            
+            <?php 
                 $actual = $cita->id;
                 $proximo = $citas[$key + 1]->id ?? 0;
 
-                if (esUltimo($actual, $proximo)) { ?>
-                    <p class="total">Total: <span>$<?php echo $total; ?></span></p>
+                if(esUltimo($actual, $proximo)) { ?>
+                    <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
 
                     <form action="/api/eliminar" method="POST">
                         <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
                         <input type="submit" class="boton-eliminar" value="Eliminar">
                     </form>
-                </li>
-            <?php }
-            } // fin de foreach 
-            ?>
-    </ul>
 
+            <?php } 
+          } // Fin de Foreach ?>
+     </ul>
 </div>
 
 <?php
