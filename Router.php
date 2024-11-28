e7147e5863ad4a766c0d252a9045246a9d57cda7
JBDolmus <jbautista.dormo.corea@gmail.com>
2024-11-27 20:18:48 -0600

    update css and router

--- Router.php
+++ Router.php
@@ -20,7 +20,8 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        debuguear($currentUrl);
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
